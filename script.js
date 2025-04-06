5b996fbbdeba57c17bc0e77c458d30164b1edff2
ppandey07 <146820527+ppandey07@users.noreply.github.com>
2025-04-06 23:42:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
+         { name: 'card1', img: 'images/tree.png' },
+        { name: 'card1', img: 'images/tree.png' },
+        { name: 'card2', img: 'images/money.png' },
+        { name: 'card2', img: 'images/money.png' },
+        { name: 'card3', img: 'images/juice.png' },
+        { name: 'card3', img: 'images/juice.png' },
+        { name: 'card4', img: 'images/owl.png' },
+        { name: 'card4', img: 'images/owl.png' },
+        { name: 'card5', img: 'images/lamp.png' },
+        { name: 'card5', img: 'images/lamp.png' },
+        { name: 'card6', img: 'images/cake.png' },
+        { name: 'card6', img: 'images/cake.png' },
+        { name: 'card7', img: 'images/bird.png' },
+        { name: 'card7', img: 'images/bird.png' },
         // ...add more pairs as needed
     ];
 
